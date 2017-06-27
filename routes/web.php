c73ef638d318dc2ef51922da7997b4498d1904a7
francisco Hernandez <francisco20990@gmail.com>
2017-06-27 19:21:12 -0400

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,14 @@ Route::get('/cursos','CursosController@cursos')->name('cursos.cursos');
 Route::get('/cursos/{id}','CursosController@display')->name('cursos.display');
 
 //LOGIN
-Route::get('/admin','FrontCrontroller@login')->name('login');
+Route::get('/login','FrontCrontroller@login')->name('login');
 Route::post('auth', 'LoginController@login')->name('auth');
 Route::post('/logout', 'LoginController@logout')->name('logout');
 
 //PANEL DE USUARIO
 Route::get('panel/dashboard', 'LoginController@index')->name('index');
-//Inscripcion
-Route::get('panel/cursos/{inscripcion}','InscripcionesController@view');
+//Cursos
+Route::get('panel/cursos/{id}','CursosController@show');
 //Pagos
 Route::resource('panel/pagos','PagosController');
 //Perfil
@@ -38,7 +38,6 @@ Route::patch('panel/perfil','UsersController@update_perfil')->name('update_perfi
 //AREA - PROFESORES
 Route::get('area/dashboard', 'LoginController@index')->name('index');
 Route::get('area/cursos/{curso}/{periodo}','CursosController@show');
-Route::patch('area/cursos/{curso}/{periodo}','CursosController@calificar');
 Route::get('area/perfil','UsersController@perfil')->name('perfil');
 Route::patch('area/perfil','UsersController@update_perfil')->name('update_perfil');
 //===========================================================================================
@@ -76,5 +75,6 @@ Route::group(['middleware' => 'auth','prefix' => 'admin'],function(){
 	Route::get('/rep_usuarios','ReportesController@usuarios')->name('pdf.usuarios');
 	Route::get('/rep_estudiantes','ReportesController@estudiantes')->name('pdf.estudiantes');
 	Route::post('/rep_pagos_fe','ReportesController@pagos_fecha')->name('pdf.pagos');
+	Route::get('/rep_periodo/{id}','ReportesController@periodos');
 
 });
